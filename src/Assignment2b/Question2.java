b7038c74315685217c4665b713f2e69f83432213
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-07 19:12:52 +1000

    Fix Question2 - Assignment2b

--- src/Assignment2b/Question2.java
+++ src/Assignment2b/Question2.java
@@ -3,15 +3,15 @@ package Assignment2b;
 public class Question2{
 
 	public static void main(String[] args) {
+		int counter = 1;
 		int j = 1;
-		while(j<21){
+		while(counter<21){
 			int num = 3*j+2;
 			if(num%4 != 0){
-				j++;
+				counter++;
 				System.out.println(num);
-			} else{
-				System.out.println("haha");
 			}
+			j++;
 		}
 	}
 
