ff4c0a37329059086dd625cb607894eea8315c52
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-20 15:10:23 +1000

    Add Assignment3 - Question1

--- src/assignment3/Question1.java
+++ src/assignment3/Question1.java
@@ -1,9 +1,29 @@
 package assignment3;
 
+import java.util.Scanner;
+
 public class Question1 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int dec = sc.nextInt();
+		int count = 0;
+		int num = dec;
+		sc.close();
+		while(dec>0){
+			dec=dec/2;
+			count++;
+			}
+		int[] ar = new int[count];
+		int i = count - 1;
+		while(num>0){
+			ar[i] = num%2;
+			num=num/2;
+			i--;
+			}
+		for(int j:ar){
+			System.out.print(j);
+		}
 	}
 
 }
