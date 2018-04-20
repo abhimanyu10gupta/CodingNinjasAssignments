3afa50eee6d3d1f6639827af8eab774722628fbc
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-20 15:45:50 +1000

    add question3

---
+++ src/assignment3/Question3.java
@@ -0,0 +1,17 @@
+package assignment3;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		int[] ar = {1,2,3,4,5,6,7,8,9,10,11};
+		for(int i = 0; i < ar.length/2; i++) {
+			int temp = ar[i];
+			ar[i] = ar[ar.length - i - 1];
+			ar[ar.length - i -1] = temp;
+		}
+		for(int i : ar){
+			System.out.print(i);
+		}
+	}
+
+}
