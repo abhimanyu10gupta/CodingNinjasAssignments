5933737d194416ca832edffefc46fa5ce25ee002
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-20 14:58:51 +1000

    Fix assignment2b - question4

--- src/Assignment2b/Question4.java
+++ src/Assignment2b/Question4.java
@@ -1,35 +1,24 @@
 package Assignment2b;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Question4 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Decimal number:");
+		Stack<Integer> st = new Stack<Integer>();
 		int dec = sc.nextInt();
-		char[] bin = new char[10];
 		sc.close();
-		int quotient = 0;
-		int div = dec;
-		int i = 0;
-		while(div > 1){
-			while(div >= 2){
-				div = div - 2;
-				quotient++;
+		while(dec>0){
+			st.push(dec%2);
+			dec=dec/2;
 			}
-				bin[i]  = Character.forDigit(div, 10); 
-				i++;
-				div = quotient;
-				if(div==1){
-					bin[i] = Character.forDigit(div, 10);
-					break;
-				}
-			
+		
+		while(!st.isEmpty()){
+			System.out.print(st.pop());
 		}
-		for(char a:bin){
-			System.out.println(a);
-		}
+		
 	}
-
 }
