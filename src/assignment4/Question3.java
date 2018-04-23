14756903fce202ca20e23d8370ba7e28356ba959
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-24 09:29:13 +1000

    Add Question3

--- src/assignment4/Question3.java
+++ src/assignment4/Question3.java
@@ -2,9 +2,20 @@ package assignment4;
 
 public class Question3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int secondLargest(int[] arr) {
+		int max = 0;
+		int sec = 0;
+		for(int i : arr){
+				if(i > max){
+					max = i;
+				}
+			}
+		for(int i : arr){
+			if(i < max && i > sec){
+				sec = i;
+			}
+		}
+		return sec; 
 	}
 
 }
