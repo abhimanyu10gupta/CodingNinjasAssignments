a1f7c967eb10cbda8feb834b75085ddc1ffe0b42
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-04-24 09:04:18 +1000

    Add question3

--- src/assignment4/Question2.java
+++ src/assignment4/Question2.java
@@ -5,7 +5,6 @@ public class Question2 {
 	public static void main(String[] args) {
 		int[] arr1 = {1,2,3,5,7,4};
 		int[] arr2 = {1,7,3,45,5,8,0,4};
-		int n = (arr1.length < arr2.length)? arr2.length:arr1.length;
 		
 		for(int i : arr1){
 			for(int j : arr2){
@@ -14,5 +13,20 @@ public class Question2 {
 				}
 			}
 		}
+	}
+	
+	public int[] question2a(int[] arr1, int[]arr2){
+		int n = (arr1.length < arr2.length)? arr2.length:arr1.length;
+		int[] ret = new int[n];
+		int k = 0;
+		for(int i : arr1){
+			for(int j : arr2){
+				if(i == j){
+					ret[k] = i;
+					k++;
+				}
+			}
+		}
+		return ret;
 	}
 }
---
+++ src/assignment4/Question3.java
@@ -0,0 +1,10 @@
+package assignment4;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
